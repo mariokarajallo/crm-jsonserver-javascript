749edd5ed49647c749141053be000d37a3f669e9
mariokarajallo <mariokarajallo92@gmail.com>
2023-05-20 16:18:39 -0400

    feat: adds mostrarClientes() functions

---
+++ js/app.js
@@ -0,0 +1,10 @@
+(function () {
+  const listado = document.querySelector("#listado-clientes");
+
+  document.addEventListener("DOMContentLoaded", mostrarClientes);
+
+  function mostrarClientes() {
+    const clientes = obtenerClientes();
+    console.log(clientes);
+  }
+})();
