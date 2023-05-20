33daa51112a71b4a73564bd21e53a157fb1e39f7
mariokarajallo <mariokarajallo92@gmail.com>
2023-05-20 16:23:00 -0400

    feat: adds obtenerClientes() expresions functions

--- js/API.js
+++ js/API.js
@@ -1,5 +1,6 @@
 const url = "http://localhost:4000/clientes";
 
+//crea un nuevo cliente en la base de datos
 export const nuevoCliente = async (cliente) => {
   try {
     await fetch(url, {
@@ -8,6 +9,17 @@ export const nuevoCliente = async (cliente) => {
       headers: { "Content-Type": "application/json" },
     });
     window.location.href = "index.html";
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// obtener clientes de la base de datos
+export const obtenerClientes = async () => {
+  try {
+    const result = await fetch(url);
+    const clientes = await result.json();
+    return clientes;
   } catch (error) {
     console.log(error);
   }
