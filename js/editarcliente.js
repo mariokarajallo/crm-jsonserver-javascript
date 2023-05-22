2403faa0ba190c1876d696e091fba51f9dac4c6f
mariokarajallo <mariokarajallo92@gmail.com>
2023-05-21 23:43:52 -0400

    feat: adds mostrarCliente functions

--- js/editarcliente.js
+++ js/editarcliente.js
@@ -1,6 +1,13 @@
 import { obtenerCliente } from "./API.js";
 
 (function () {
+  //datos del formulario
+  const inputNombre = document.querySelector("#nombre");
+  const inputEmail = document.querySelector("#email");
+  const inputTelefono = document.querySelector("#telefono");
+  const inputEmpresa = document.querySelector("#empresa");
+  const inputId = document.querySelector("#id");
+
   document.addEventListener("DOMContentLoaded", async () => {
     //consultar que registros estamos visitando para hacer la consulta a la api y traer los resultados
     const parametrosURL = new URLSearchParams(window.location.search);
@@ -9,6 +16,16 @@ import { obtenerCliente } from "./API.js";
 
     const cliente = await obtenerCliente(idCliente);
 
-    console.log(cliente);
+    mostrarCliente(cliente);
   });
+
+  function mostrarCliente(cliente) {
+    const { nombre, email, telefono, empresa, id } = cliente;
+
+    inputNombre.value = nombre;
+    inputEmail.value = email;
+    inputTelefono.value = telefono;
+    inputEmpresa.value = empresa;
+    inputId.value = id;
+  }
 })();
